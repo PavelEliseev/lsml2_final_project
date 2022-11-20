a46f33059c9681e02dab8f23c3e1748ef57de570
Pavel Eliseev <eliseevpaaa@gmail.com>
2022-11-20 11:36:11 +0300

    Update model.py

--- data/model.py
+++ data/model.py
@@ -5,13 +5,13 @@ import transformers
 class BERTBaseUncased(nn.Module):
     def __init__(self):
         super(BERTBaseUncased, self).__init__()
-        self.model = transformers.BertModel.from_pretrained("bert-base-uncased", return_dict=False)
-        self.model_drop = nn.Dropout(0.3)
-        self.linear = nn.Linear(768, 1)
+        self.bert = transformers.BertModel.from_pretrained("bert-base-uncased", return_dict=False)
+        self.bert_drop = nn.Dropout(0.3)
+        self.out = nn.Linear(768, 1)
 
     def forward(self, ids, mask, token_type_ids):
-        out_1, out_2 = self.model(ids,
+        out_1, out_2 = self.bert(ids,
                                   attention_mask=mask,
                                   token_type_ids=token_type_ids
                                   )
-        return self.linear(self.model_drop(out_2))
+        return self.out(self.bert_drop(out_2))
